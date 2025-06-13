8c7d1d2979611f2b7501089de38897ea4f73ebf1
elurg <elurg@gmail.com>
2025-06-13 19:34:38 +0200

    CrossOrigin en StudentController

--- campus/src/main/java/com/cuatico/campus/controllers/StudentController.java
+++ campus/src/main/java/com/cuatico/campus/controllers/StudentController.java
@@ -3,6 +3,7 @@ package com.cuatico.campus.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/students")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class StudentController {
 
 	private final StudentRepository repo;
